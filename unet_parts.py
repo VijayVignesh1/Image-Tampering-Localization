75817a6b242fabb5644bb5b3ff463ded7dea063b
VijayVignesh1 <vijaydhonindia@gmail.com>
2019-11-19 21:56:32 +0530

    Update unet_parts.py

--- unet_parts.py
+++ unet_parts.py
@@ -1,12 +1,9 @@
-# sub-parts of the U-Net model
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 
 class double_conv(nn.Module):
-    '''(conv => BN => ReLU) * 2'''
     def __init__(self, in_ch, out_ch):
         super(double_conv, self).__init__()
         self.conv = nn.Sequential(
@@ -40,7 +37,6 @@ class down(nn.Module):
             nn.MaxPool2d(2),
             double_conv(in_ch, out_ch)
         )
-
     def forward(self, x):
         x = self.mpconv(x)
         return x
@@ -49,30 +45,19 @@ class down(nn.Module):
 class up(nn.Module):
     def __init__(self, in_ch, out_ch, bilinear=True):
         super(up, self).__init__()
-
-        #  would be a nice idea if the upsampling could be learned too,
-        #  but my machine do not have enough memory to handle all those weights
         if bilinear:
             self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
         else:
             self.up = nn.ConvTranspose2d(in_ch//2, in_ch//2, 2, stride=2)
-
         self.conv = double_conv(in_ch, out_ch)
 
     def forward(self, x1, x2):
         x1 = self.up(x1)
-        
-        # input is CHW
         diffY = x2.size()[2] - x1.size()[2]
         diffX = x2.size()[3] - x1.size()[3]
 
         x1 = F.pad(x1, (diffX // 2, diffX - diffX//2,
                         diffY // 2, diffY - diffY//2))
-        
-        # for padding issues, see 
-        # https://github.com/HaiyongJiang/U-Net-Pytorch-Unstructured-Buggy/commit/0e854509c2cea854e247a9c615f175f76fbb2e3a
-        # https://github.com/xiaopeng-liao/Pytorch-UNet/commit/8ebac70e633bac59fc22bb5195e513d5832fb3bd
-
         x = torch.cat([x2, x1], dim=1)
         x = self.conv(x)
         return x
@@ -82,7 +67,6 @@ class outconv(nn.Module):
     def __init__(self, in_ch, out_ch):
         super(outconv, self).__init__()
         self.conv = nn.Conv2d(in_ch, out_ch, 1)
-
     def forward(self, x):
         x = self.conv(x)
         return x
